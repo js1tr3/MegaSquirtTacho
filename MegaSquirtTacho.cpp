f12ec3cdf2a0b8fbe3860c0b9f7037d02eb1bf97
Jason Siegel <siegeljb@gmail.com>
2022-09-13 22:08:09 -0400

    working calibration duty to rpm

--- MegaSquirtTacho.cpp
+++ MegaSquirtTacho.cpp
@@ -9,40 +9,42 @@
 #include "hardware/watchdog.h"
 #include "hardware/pwm.h"
 #include "include/mcp2515/mcp2515.h"
+#include "hardware/clocks.h"
 
 MCP2515 can0;
 struct can_frame rx;
 // UART defines
 // By default the stdout UART is `uart0`, so we will use the second one
-#define UART_ID uart1
-#define BAUD_RATE 9600
+//#define UART_ID uart1
+//#define BAUD_RATE 9600
 
 // Use pins 4 and 5 for UART1
 // Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
-#define UART_TX_PIN 4
-#define UART_RX_PIN 5
+//#define UART_TX_PIN 4
+//#define UART_RX_PIN 5
 
 // GPIO defines
 // Example uses GPIO 2
-#define GPIO 2
+#define GPIO 20
+#define GPIO_O 18
 
 
 // SPI Defines
 // We are going to use SPI 0, and allocate it to the following GPIO pins
 // Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
 #define SPI_PORT spi0
-#define PIN_MISO 16
-#define PIN_CS   17
-#define PIN_SCK  18
-#define PIN_MOSI 19
+#define PIN_MISO 4
+#define PIN_CS   9
+#define PIN_SCK  2
+#define PIN_MOSI 13
 
 #define Tacho_PIN 24
 // I2C defines
 // This example will use I2C0 on GPIO8 (SDA) and GPIO9 (SCL) running at 400KHz.
 // Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
-#define I2C_PORT i2c0
-#define I2C_SDA 8
-#define I2C_SCL 9
+#define I2C_PORT i2c1
+#define I2C_SDA 6
+#define I2C_SCL 7
 
 int64_t alarm_callback(alarm_id_t id, void *user_data) {
     // Put your timeout handler code in here
@@ -55,30 +57,44 @@ int64_t alarm_callback(alarm_id_t id, void *user_data) {
 int main()
 {
     __uint16_t RPM=0;
-
+    const uint32_t f_pwm = 200; // frequency we want to generate
+	uint16_t duty = 60; // duty cycle, in percent
     stdio_init_all();
 
     // Set up our UART
-    uart_init(UART_ID, BAUD_RATE);
+    //uart_init(UART_ID, BAUD_RATE);
     // Set the TX and RX pins by using the function select on the GPIO
     // Set datasheet for more information on function select
-    gpio_set_function(UART_TX_PIN, GPIO_FUNC_UART);
-    gpio_set_function(UART_RX_PIN, GPIO_FUNC_UART);
-    
+    //gpio_set_function(UART_TX_PIN, GPIO_FUNC_UART);
+    //gpio_set_function(UART_RX_PIN, GPIO_FUNC_UART);
+
 
     // Tell the tacho pin that the PWM is in charge of its value.
     gpio_set_function(Tacho_PIN, GPIO_FUNC_PWM);
     // Figure out which slice we just connected to the LED pin
     uint slice_num = pwm_gpio_to_slice_num(Tacho_PIN);
 
-    // Get some sensible defaults for the slice configuration. By default, the
-    // counter is allowed to wrap over its maximum range (0 to 2**16-1)
-    pwm_config config = pwm_get_default_config();
-    // Set divider, reduces counter clock to sysclock/this value
-    pwm_config_set_clkdiv(&config, 1.f);
-    // Load the configuration into our PWM slice, and set it running.
-    pwm_init(slice_num, &config, true);
-    pwm_set_gpio_level(Tacho_PIN, 0);
+    // // Get some sensible defaults for the slice configuration. By default, the
+    // // counter is allowed to wrap over its maximum range (0 to 2**16-1)
+    // pwm_config config = pwm_get_default_config();
+    // // Set divider, reduces counter clock to sysclock/this value
+    // pwm_config_set_clkdiv(&config, 1.f);
+    // // Load the configuration into our PWM slice, and set it running.
+    // pwm_init(slice_num, &config, true);
+    // pwm_set_gpio_level(Tacho_PIN, 0);
+
+// determine top given Hz - assumes free-running counter rather than phase-correct
+	uint32_t  f_sys = clock_get_hz(clk_sys); // typically 125'000'000 Hz
+	float divider = f_sys / 1'000'000UL;  // let's arbitrarily choose to run pwm clock at 1MHz
+	pwm_set_clkdiv(slice_num, divider); // pwm clock should now be running at 1MHz
+	uint32_t top =  1'000'000UL/f_pwm -1; // TOP is u16 has a max of 65535, being 65536 cycles
+	pwm_set_wrap(slice_num, top);
+
+	// set duty cycle
+	uint16_t level = (top+1) * duty / 100 -1; // calculate channel level from given duty cycle in %
+	pwm_set_chan_level(slice_num, 0, level); 
+	
+	pwm_set_enabled(slice_num, true); // let's go!
 
 
     // GPIO initialisation.
@@ -86,7 +102,8 @@ int main()
     gpio_init(GPIO);
     gpio_set_dir(GPIO, GPIO_IN);
     gpio_pull_up(GPIO);
-    
+    gpio_init(GPIO_O);
+    gpio_set_dir(GPIO_O, GPIO_OUT);
 
     // Example of using the HW divider. The pico_divider library provides a more user friendly set of APIs 
     // over the divider (and support for 64 bit divides), and of course by default regular C language integer
@@ -126,14 +143,27 @@ int main()
     // Timer example code - This example fires off the callback after 2000ms
     add_alarm_in_ms(2000, alarm_callback, NULL, false);
 
-
+    sleep_ms(3000); // 0.5s delay
     puts("Hello, world!");
-    stdio_init_all();
 
-    //Initialize interface
+        //Initialize interface
     can0.reset();
     can0.setBitrate(CAN_500KBPS, MCP_16MHZ);
     can0.setNormalMode();
+
+    //     while(1){
+    //     gpio_put(GPIO_O, 1); // Set pin 25 to high
+    //     printf("LED ON!\n");
+    //     sleep_ms(1000); // 0.5s delay
+
+    //     gpio_put(GPIO_O, 0); // Set pin 25 to low
+    //     printf("LED OFF!\n");
+    //     sleep_ms(1000); // 0.5s delay
+    // }
+
+
+
+
 
     //Listen loop
     while(true) {
@@ -142,35 +172,73 @@ int main()
 
         }
         int16_t ch = getchar_timeout_us(100);
-        while (ch != PICO_ERROR_TIMEOUT) {
+        if(ch != PICO_ERROR_TIMEOUT) {
            // uart_putc_raw(UART0_ID, ch);    // Send to UART0
            // uart_putc_raw(UART1_ID, ch);    // Send to UART1
-           // printf("%c", ch);               // Echo back so you can see what you've done
-            ch = getchar_timeout_us(100);
-        }
-        switch(ch){
-        case 1:
-            pwm_set_gpio_level(Tacho_PIN, 1000);
-            printf("PWM: %d\n",1000);
-        break;
-                case 2:
-            pwm_set_gpio_level(Tacho_PIN, 10000);
-            printf("PWM: %d\n",10000);
-        break;
-                case 3:
-            pwm_set_gpio_level(Tacho_PIN, 20000);
-            printf("PWM: %d\n",20000);
-        break;
-                case 4:
-            pwm_set_gpio_level(Tacho_PIN, 30000);
-            printf("PWM: %d\n",30000);
-        break;
-                case 5:
-            pwm_set_gpio_level(Tacho_PIN, 50000);
-        break;
-                case 0:
-            pwm_set_gpio_level(Tacho_PIN, 0);
-        break;
+           //                // Echo back so you can see what you've done
+            //ch = getchar_timeout_us(100);
+            printf("Got %c, val %d \n", ch, ch);
+        
+
+            switch(ch){
+            case '\n':
+                duty=-1;
+                break;
+            case 48:
+                duty=0;
+                break;
+            case 49:
+                duty=10;
+                break;
+            //break;
+            case 50:
+                duty=20;
+                break;
+              
+            //break;
+            case 51:
+                duty=30;
+                break;
+            //break;
+            case 52:
+                duty=40;
+                break;
+            //break;
+            case 53:
+                duty=50;
+                break;
+            case 54:
+                duty=60;
+                break;
+            case 55:
+                duty=70;
+                break;
+            case 56:
+                duty=80;
+                break;            
+            case 57:
+                duty=90;
+                break;
+            case 58:
+                duty=100;
+                break;
+            //break;
+            default:
+                duty=100;
+            //break;
+            }
+            if(duty>0 & duty<=100)
+            {
+            	// set duty cycle
+                uint16_t level = (top+1) * duty / 100 -1; // calculate channel level from given duty cycle in %
+                pwm_set_chan_level(slice_num, 0, level);
+                printf("PWM: %d duty and level=%d\n",duty,level);
+            }
+            if(duty==0){
+                uint16_t level = 0; // calculate channel level from given duty cycle in %
+                pwm_set_chan_level(slice_num, 0, level);
+                printf("PWM: %d duty and level=%d\n",duty,level);
+            }
         }
     }
 
